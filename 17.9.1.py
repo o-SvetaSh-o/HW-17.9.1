0de605f68b161c134960a701b83348a331ee3079
unknown <C:\Users\ \AppData\Roaming\The Bat!>
2023-03-28 18:31:09 +0300

    A new file added

--- 17.9.1.py
+++ 17.9.1.py
@@ -1,48 +1,52 @@
+#задаем массив вещественных чисел и проверяем его
 try:
     array = [float(x) for x in input("Введите числа в любом порядке, через пробел: ").split()]
 except ValueError:
     print('В введенной последовательности чисел ошибка')
 
-
+# сортируем массив методом быстрой сортировки
 def quick_sort(array):
-    quick_sort_helper(array, 0, len(array) - 1)
-    return array
+    quick_sort_helper(array, 0, len(array) - 1) #использует неотсортированный массив, начальный индекс и конечный индекс
+    return array #возвращает отсортированный массив
 
+# функция принимает неотсортированный массив, начальный и конечный индекс массива, разбивает массив на подмассивы
 def quick_sort_helper(array, low, high):
     if low < high:
-        split_point = partition(array, low, high)
-        quick_sort_helper(array, low, split_point - 1)
-        quick_sort_helper(array, split_point + 1, high)
+        split_point = partition(array, low, high) #переменная split_point вызывает функцию partition
+        quick_sort_helper(array, low, split_point - 1)#рекурсивно вызываем функцию для одной части массива
+        quick_sort_helper(array, split_point + 1, high)#рекурсивно вызываем функцию для второй части массива
 
+# функция сравнивает левую метку и правую метку со значением опорного элемента
 def partition(array, low, high):
-    pivot_value = array[low]
-    left_mark = low + 1
-    right_mark = high
-    done = False
+    pivot_value = array[low] #зададим переменную опорная метка и присваиваем ей первое значение в массиве
+    left_mark = low + 1 #задаем переменную левая метка, это будет второй элемент в массиве
+    right_mark = high #задаем переменную правая метка, это будет последний элемент массива
+    done = False #переменная для завершения цикла while not done
 
-    while not done:
-        while left_mark <= right_mark and array[left_mark] <= pivot_value:
+    while not done: #цикл работает, пока done не примет значение True
+        while left_mark <= right_mark and array[left_mark] <= pivot_value: #цикл передвижения левой метки
             left_mark += 1
-        while right_mark >= left_mark and array[right_mark] >= pivot_value:
+        while right_mark >= left_mark and array[right_mark] >= pivot_value: # цикл передвижения правой метки
             right_mark -= 1
 
-        if right_mark < left_mark:
+        if right_mark < left_mark: #если значение правой метки пересечет значение левой метки, то цикл прерывается
             done = True
         else:
-            array[left_mark], array[right_mark] = array[right_mark], array[left_mark]
+            array[left_mark], array[right_mark] = array[right_mark], array[left_mark]# в противном случае, меняеи местами значания левой и правой метки
 
-    array[low], array[right_mark] = array[right_mark], array[low]
+    array[low], array[right_mark] = array[right_mark], array[low]# если цикл прерывается, то меняются местами значение опорной точки и правого элемента
     return right_mark
 
 
 print("Исходный массив: ", array)
 result = quick_sort(array)
-print("Результат сортировки:", result)
+print("Отсортированный массив:", result)
 
 try:
     element = float(input('Введите число: '))
 except ValueError:
         print('Это не число')
+
 
 def binary_search(result, element, left, right):
     if left > right:  # если левая граница превысила правую,
